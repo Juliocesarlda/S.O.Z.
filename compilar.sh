11f02ddec6e7724c311c846fac47950a5303f214
Julio Cesar <juliolondrina_32@hotmail.com>
2026-07-28 12:55:13 -0300

    Add files via upload

--- compilar.sh
+++ compilar.sh
@@ -1,25 +1,28 @@
-#!/bin/bash 
+#!/bin/bash
 
 echo "========================================="
 echo "   LIMPANDO ARQUIVOS VELHOS DO SISTEMA   "
 echo "========================================="
-rm -f bootloader.bin boot.o kernel.o shell.o screen.o keyboard.o kernel.bin jcos.img julio_cesar_novo.img
+rm -f bootloader.bin boot.o kernel.o shell.o screen.o keyboard.o kernel.bin idt.o irq.o interrupts.o jcos.img julio_cesar_novo.img
 
 echo "========================================="
 echo "        COMPILANDO O JULIO CESAR OS      "
 echo "========================================="
 
-# 0. Verifica suporte a 32-bits (Corrigido com a flag -c)
+# 0. Verifica suporte a 32-bits
 if ! gcc -m32 -c -x c /dev/null -o /dev/null 2>/dev/null; then
     echo "ERRO: gcc-multilib nao instalado!"
     echo "Execute: sudo apt install gcc-multilib"
     exit 1
 fi
 
-# 1. Bootloader 16-bits (NASM)
+# 1. Bootloader 16-bits e Stubs de Interrupção em Assembly (NASM)
 nasm -f bin bootloader.s -o bootloader.bin
 if [ $? -ne 0 ]; then echo "Erro no Bootloader!"; exit 1; fi
 echo "bootloader.bin: $(wc -c < bootloader.bin) bytes"
+
+nasm -f elf32 interrupts.s -o interrupts.o
+if [ $? -ne 0 ]; then echo "Erro em interrupts.s!"; exit 1; fi
 
 # 2. Ponto de entrada do kernel (Assembly 32-bits via GCC)
 gcc -m32 -c boot.s -o boot.o
@@ -44,8 +47,16 @@ echo "Compilando keyboard.c..."
 gcc $FLAGS keyboard.c -o keyboard.o
 if [ $? -ne 0 ]; then echo "Erro no keyboard.c!"; exit 1; fi
 
+echo "Compilando idt.c..."
+gcc $FLAGS idt.c -o idt.o
+if [ $? -ne 0 ]; then echo "Erro no idt.c!"; exit 1; fi
+
+echo "Compilando irq.c..."
+gcc $FLAGS irq.c -o irq.o
+if [ $? -ne 0 ]; then echo "Erro no irq.c!"; exit 1; fi
+
 # 4. Linka em binário PURO (sem header ELF)
-ld -m elf_i386 -T linker.ld boot.o kernel.o shell.o screen.o keyboard.o -o kernel.bin --oformat binary
+ld -m elf_i386 -T linker.ld boot.o kernel.o shell.o screen.o keyboard.o idt.o irq.o interrupts.o -o kernel.bin --oformat binary
 if [ $? -ne 0 ]; then echo "Erro na Linkagem!"; exit 1; fi
 echo "kernel.bin: $(wc -c < kernel.bin) bytes"
 
